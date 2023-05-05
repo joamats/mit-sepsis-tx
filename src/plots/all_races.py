5f2a6e0768c9e23cd0e896659168602d735e07d6
João Matos <joaocarlosm00@gmail.com>
2023-05-06 00:45:27 +0100

    Plot functions & LogReg main results

--- src/plots/all_races.py
+++ src/plots/all_races.py
@@ -1,85 +1,88 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-import numpy as np
-from tqdm import tqdm
 
 import matplotlib
 matplotlib.use('TKAgg')
 
-filename = "logreg_cv_all_coh_races"
-model_name = "Logistic Regression"
+
+def plot_results(filename, model_name):
+
+    # load data from csv file into pandas dataframe
+    data = pd.read_csv(f"results/models/{filename}.csv")
+
+    # group data by treatment type
+    treatment_groups = data.groupby("treatment")
+
+    races = ['race_white','race_black', 'race_hisp', 'race_asian']
+    races_names = ['White','Black', 'Hispanic', 'Asian']
+    ws = [.6, .2, -.2, -.6,]
+
+    # Set the figure and axes
+    fig, axes = plt.subplots(1, 3,
+                            sharex=True, sharey=True,
+                            figsize=(12,4.5))
+
+    fig.suptitle(f'{model_name} Model: Likelihood of Treatment Initiation')
+
+    # create dictionary of name for each treatment group
+    treatment_names = {"MV_elig": "Mechanical Ventilation",
+                    "RRT_elig": "RRT",
+                    "VP_elig": "Vasopressor(s)"
+    }
+
+    # set common horizontal line at 1 for all subplots
+    for i, ax in enumerate(axes):
+        ax.axvline(x=1, linewidth=0.8, linestyle='--', color='black')
+
+    # loop over treatment groups and create a subplot for each
+    for (treatment, groups), ax in zip(treatment_groups, axes):
+        # plot odds ratios with confidence intervals as error bars
+        for i in range(len(races)):
+
+            name = races_names[i]
+            w = ws[i]
+            group = groups[groups.race == races[i]]
+
+            ax.errorbar(group["OR"],
+                        group["cohort"]*3 + w,
+                        xerr=[group["OR"] - group["2.5%"], group["97.5%"] - group["OR"]],
+                        label=name,
+                        fmt='o',
+                        linewidth=.5,
+                        capsize=3)
+        # set subplot title
+        ax.set_title([treatment_names[treatment]][0])
+        # set yrange to breath a bit
+        ax.set_ylim([1.5, 13.5])
+        # set xrange to
+        ax.set_xlim([.5, 1.5])
+        # set yticks in string format
+        ax.set_yticks([3,6,9,12])
+        ax.set_yticklabels(["0 - 1", "0 - 2", "0 - 3", "0 - 4"])
+        ax.set_xlabel("Favours White | Favours R.E.G",
+                    fontsize=8, labelpad=5, color='gray')
+        
+    # add horizontal lines to all axes, per y tick
+    for ax in axes:
+        for y in [3,6,9]:
+            ax.axhline(y+1.5, color='gray', linestyle='-', linewidth=.5)
+        
+
+    # add legend
+    axes[2].legend(loc='upper left', bbox_to_anchor=(1.05, .7),
+                title="Race-Ethnicity")
+    # add title to legend
+
+    fig.supxlabel('Odds Ratio (95% CI)')
+    fig.supylabel('Elegibility Window (days)\n')
+    plt.tight_layout()
 
 
-# load data from csv file into pandas dataframe
-data = pd.read_csv(f"results/models/{filename}.csv")
+    # Save the figure
+    fig.savefig(f"results/plots/{filename}.png", dpi=300, bbox_inches="tight")
 
-# group data by treatment type
-treatment_groups = data.groupby("treatment")
+filenames = ["logreg_cv_all_coh_races", "xgb_cv_all_coh_races"]
+model_names = ["Logistic Regression", "XGBoost"]
 
-races = ['race_white','race_black', 'race_hisp', 'race_asian']
-races_names = ['White','Black', 'Hispanic', 'Asian']
-ws = [.6, .2, -.2, -.6,]
-
-# Set the figure and axes
-fig, axes = plt.subplots(1, 3,
-                         sharex=True, sharey=True,
-                         figsize=(12,4.5))
-
-fig.suptitle(f'{model_name} Model: Likelihood of Treatment Initiation')
-
-# create dictionary of name for each treatment group
-treatment_names = {"MV_elig": "Mechanical Ventilation",
-                   "RRT_elig": "RRT",
-                   "VP_elig": "Vasopressor(s)"
-}
-
-# set common horizontal line at 1 for all subplots
-for i, ax in enumerate(axes):
-    ax.axvline(x=1, linewidth=0.8, linestyle='--', color='black')
-
-# loop over treatment groups and create a subplot for each
-for (treatment, groups), ax in zip(treatment_groups, axes):
-    # plot odds ratios with confidence intervals as error bars
-    for i in range(len(races)):
-
-        name = races_names[i]
-        w = ws[i]
-        group = groups[groups.race == races[i]]
-
-        ax.errorbar(group["OR"],
-                    group["cohort"]*3 + w,
-                    xerr=[group["OR"] - group["2.5%"], group["97.5%"] - group["OR"]],
-                    label=name,
-                    fmt='o',
-                    linewidth=.5,
-                    capsize=3)
-    # set subplot title
-    ax.set_title([treatment_names[treatment]][0])
-    # set yrange to breath a bit
-    ax.set_ylim([1.5, 13.5])
-    # set xrange to
-    ax.set_xlim([.5, 1.5])
-    # set yticks in string format
-    ax.set_yticks([3,6,9,12])
-    ax.set_yticklabels(["0 - 1", "0 - 2", "0 - 3", "0 - 4"])
-    ax.set_xlabel("Favours White | Favours R.E.G",
-                fontsize=8, labelpad=5, color='gray')
-    
-# add horizontal lines to all axes, per y tick
-for ax in axes:
-    for y in [3,6,9]:
-        ax.axhline(y+1.5, color='gray', linestyle='-', linewidth=.5)
-    
-
-# add legend
-axes[2].legend(loc='upper left', bbox_to_anchor=(1.05, .7),
-               title="Race-Ethnicity")
-# add title to legend
-
-fig.supxlabel('Odds Ratio (95% CI)')
-fig.supylabel('Elegibility Window (days)\n')
-plt.tight_layout()
-
-
-# Save the figure
-fig.savefig(f"results/plots/{filename}.png", dpi=300, bbox_inches="tight")
+for f, m in zip(filenames, model_names):
+    plot_results(f, m)
--- src/plots/white_nonwhite.py
+++ src/plots/white_nonwhite.py
@@ -1,61 +1,64 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-import numpy as np
-from tqdm import tqdm
 
 import matplotlib
 matplotlib.use('TKAgg')
 
-filename = "xgb_cv_all_coh"
-model_name = "XGBoost"
+def plot_results(filename, model_name):
+    # load data from csv file into pandas dataframe
+    data = pd.read_csv(f"results/models/{filename}.csv")
 
-# load data from csv file into pandas dataframe
-data = pd.read_csv(f"results/models/{filename}.csv")
+    # group data by treatment type
+    treatment_groups = data.groupby("treatment")
 
-# group data by treatment type
-treatment_groups = data.groupby("treatment")
+    # Set the figure and axes
+    fig, axes = plt.subplots(1, 3,
+                            sharex=True, sharey=True,
+                            figsize=(9,3.5))
 
-# Set the figure and axes
-fig, axes = plt.subplots(1, 3,
-                         sharex=True, sharey=True,
-                         figsize=(9,3.5))
+    fig.suptitle(f'{model_name} Model: Likelihood of Treatment Initiation')
 
-fig.suptitle(f'{model_name} Model: Likelihood of Treatment Initiation')
+    # create dictionary of name for each treatment group
+    treatment_names = {"MV_elig": "Mechanical Ventilation",
+                    "RRT_elig": "RRT",
+                    "VP_elig": "Vasopressor(s)"
+    }
 
-# create dictionary of name for each treatment group
-treatment_names = {"MV_elig": "Mechanical Ventilation",
-                   "RRT_elig": "RRT",
-                   "VP_elig": "Vasopressor(s)"
-}
+    # set common horizontal line at 1 for all subplots
+    for ax in axes:
+        ax.axvline(x=1, linewidth=0.8, linestyle='--', color='black')
 
-# set common horizontal line at 1 for all subplots
-for ax in axes:
-    ax.axvline(x=1, linewidth=0.8, linestyle='--', color='black')
-
-# loop over treatment groups and create a subplot for each
-for (treatment, group), ax in zip(treatment_groups, axes):
-    # plot odds ratios with confidence intervals as error bars
-    ax.errorbar(x=group["OR"],
-                y=group["cohort"],
-                xerr=[group["OR"] - group["2.5%"], group["97.5%"] - group["OR"]],
-                fmt='o',
-                capsize=3)
-    # set subplot title
-    ax.set_title([treatment_names[treatment]][0])
-    # set xrange to breath a bit
-    ax.set_ylim([.5, 4.5])
-    # set yrange to
-    ax.set_xlim([0.5, 1.5])
-    # set xticks in string format
-    ax.set_yticks([1,2,3,4])
-    ax.set_yticklabels(["0 - 1", "0 - 2", "0 - 3", "0 - 4"])
-    ax.set_xlabel("Favours White | Favours R.E.G",
-                  fontsize=8, labelpad=5, color='gray')
+    # loop over treatment groups and create a subplot for each
+    for (treatment, group), ax in zip(treatment_groups, axes):
+        # plot odds ratios with confidence intervals as error bars
+        ax.errorbar(x=group["OR"],
+                    y=group["cohort"],
+                    xerr=[group["OR"] - group["2.5%"], group["97.5%"] - group["OR"]],
+                    fmt='o',
+                    capsize=3)
+        # set subplot title
+        ax.set_title([treatment_names[treatment]][0])
+        # set xrange to breath a bit
+        ax.set_ylim([.5, 4.5])
+        # set yrange to
+        ax.set_xlim([0.5, 1.5])
+        # set xticks in string format
+        ax.set_yticks([1,2,3,4])
+        ax.set_yticklabels(["0 - 1", "0 - 2", "0 - 3", "0 - 4"])
+        ax.set_xlabel("Favours White | Favours R.E.G",
+                    fontsize=8, labelpad=5, color='gray')
 
 
-fig.supxlabel('Odds Ratio (95% CI)')
-fig.supylabel('Elegibility Window (days)')
-plt.tight_layout()
+    fig.supxlabel('Odds Ratio (95% CI)')
+    fig.supylabel('Elegibility Window (days)')
+    plt.tight_layout()
 
-# Save the figure
-fig.savefig(f"results/plots/{filename}.png", dpi=300, bbox_inches="tight")
+    # Save the figure
+    fig.savefig(f"results/plots/{filename}.png", dpi=300, bbox_inches="tight")
+
+
+filenames = ["logreg_cv_all_coh", "xgb_cv_all_coh"]
+model_names = ["Logistic Regression", "XGBoost"]
+
+for f, m in zip(filenames, model_names):
+    plot_results(f, m)
